368d6b5bb49448b5fd44d6283a49a0355ac6baac
BearandYoon <luckysevenbear@gmail.com>
2018-01-23 22:22:09 +0200

    Created loading indicator with custom css
    How to use:
    - import LoadingIndicatorModule to the module
    - inline <loading-indicator [title]="'Loading...'"></loading-indicator> where to use

--- src/app/shared/ui/smartadmin-ui.module.ts
+++ src/app/shared/ui/smartadmin-ui.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
-import {SmartProgressbarModule} from "./smart-progressbar/smart-progressbar.module";
-import {TreeViewModule} from "./tree-view/tree-view.module";
-import {JqueryUiModule} from "./jquery-ui/jquery-ui.module";
-import {NestableListModule} from "./nestable-list/nestable-list.module";
+// import {SmartProgressbarModule} from "./smart-progressbar/smart-progressbar.module";
+// import {TreeViewModule} from "./tree-view/tree-view.module";
+// import {JqueryUiModule} from "./jquery-ui/jquery-ui.module";
+// import {NestableListModule} from "./nestable-list/nestable-list.module";
+import {LoadingIndicatorModule} from "./loading-indicator/loading-indicator.module";
 
 @NgModule({
   imports: [CommonModule],
 
-  exports: [SmartProgressbarModule, JqueryUiModule, NestableListModule, TreeViewModule],
+  exports: [LoadingIndicatorModule],
+  // exports: [SmartProgressbarModule, JqueryUiModule, NestableListModule, TreeViewModule, LoadingIndicatorModule],
 })
 export class SmartadminUiModule{}
