cb2a50553c049a14b5473104e516928d0e469c36
BearandYoon <luckysevenbear@gmail.com>
2018-01-26 05:26:39 +0400

    env files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://api.amalyze.com/' + this.apiVersion,
+  baseUrl: 'https://api.amalyze.com/0.0.6',
   apiVersion: '0.0.6',
   version: '0.0.4',
   googleSiteKey: '6LcKNEIUAAAAAPGe3VDGzUcqXyi5iyMD-IPhiXej'
