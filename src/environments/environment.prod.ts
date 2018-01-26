9cf7df6537af9159c05b22faf795d79f749a6595
BearandYoon <luckysevenbear@gmail.com>
2018-01-26 04:52:25 +0400

    env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
-  production: true,
-  baseUrl: 'https://api.amalyze.com/0.0.1',
-  googleSiteKey: '6LcKNEIUAAAAAPGe3VDGzUcqXyi5iyMD-IPhiXej'
+    production: true,
+    baseUrl: 'https://api.amalyze.com/0.0.6',
+    apiVersion: '0.0.6',
+    version: '0.0.4',
+    googleSiteKey: '6LcKNEIUAAAAAPGe3VDGzUcqXyi5iyMD-IPhiXej'
 };
