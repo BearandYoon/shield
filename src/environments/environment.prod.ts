84f1742901045ee09df6fa7d05c13f63e21166a2
BearandYoon <luckysevenbear@gmail.com>
2018-01-30 13:24:53 +0200

    navigation fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://api.amalyze.com/0.0.9',
-    apiVersion: '0.0.9',
+    baseUrl: 'https://api.amalyze.com/0.0.10',
+    apiVersion: '0.0.10',
     version: '0.0.6',
     googleSiteKey: '6LcKNEIUAAAAAPGe3VDGzUcqXyi5iyMD-IPhiXej'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://api.amalyze.com/0.0.9',
-  apiVersion: '0.0.9',
+  baseUrl: 'https://api.amalyze.com/0.0.10',
+  apiVersion: '0.0.10',
   version: '0.0.6',
   googleSiteKey: '6LcKNEIUAAAAAPGe3VDGzUcqXyi5iyMD-IPhiXej'
 };
