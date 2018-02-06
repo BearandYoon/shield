a48fd508113cae3b6082d7b2873a15575995c8a6
BearandYoon <luckysevenbear@gmail.com>
2018-02-06 16:36:03 +0400

    update versions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     baseUrl: 'https://api.amalyze.com/0.0.13',
-    apiVersion: '0.0.10',
-    version: '0.0.6',
+    apiVersion: '0.0.13',
+    version: '0.0.7',
     googleSiteKey: '6LcKNEIUAAAAAPGe3VDGzUcqXyi5iyMD-IPhiXej'
 };
