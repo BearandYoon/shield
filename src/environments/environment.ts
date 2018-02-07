bcde7d44f2a6bce0ec4900a0ef0d2f691eb1e762
BearandYoon <luckysevenbear@gmail.com>
2018-02-08 02:40:18 +0400

    update to api version 17

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://api.amalyze.com/0.0.15',
-  apiVersion: '0.0.15',
+  baseUrl: 'https://api.amalyze.com/0.0.17',
+  apiVersion: '0.0.17',
   version: '0.0.8',
   googleSiteKey: '6LcKNEIUAAAAAPGe3VDGzUcqXyi5iyMD-IPhiXej'
 };
