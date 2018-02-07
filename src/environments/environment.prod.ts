633f46e55572da046421cea36beabeb06d35e72b
BearandYoon <luckysevenbear@gmail.com>
2018-02-07 04:46:21 +0400

    roll back to previous api version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://api.amalyze.com/0.0.16',
-    apiVersion: '0.0.16',
+    baseUrl: 'https://api.amalyze.com/0.0.15',
+    apiVersion: '0.0.15',
     version: '0.0.8',
     googleSiteKey: '6LcKNEIUAAAAAPGe3VDGzUcqXyi5iyMD-IPhiXej'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://api.amalyze.com/0.0.16',
-  apiVersion: '0.0.16',
+  baseUrl: 'https://api.amalyze.com/0.0.15',
+  apiVersion: '0.0.15',
   version: '0.0.8',
   googleSiteKey: '6LcKNEIUAAAAAPGe3VDGzUcqXyi5iyMD-IPhiXej'
 };
