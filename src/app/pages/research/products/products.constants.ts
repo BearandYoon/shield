fb8543825b4785f93427e25f8f57cc4a732bed09
BearandYoon <luckysevenbear@gmail.com>
2018-02-07 14:57:58 +0100

    add product constant file

---
+++ src/app/pages/research/products/products.constants.ts
@@ -0,0 +1,12 @@
+const TYPES = {
+    SPA: 'SPA'
+}
+
+const PROGRESS = {
+    NEW: 'NEW',
+    PROCESSED: 'PROCESSED',
+    ERROR: 'ERROR',
+    PROCESSING: 'PROCESSING'
+}
+
+export { TYPES, PROGRESS }
