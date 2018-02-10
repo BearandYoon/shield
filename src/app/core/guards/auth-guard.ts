94d2ce7422ee888307de92ed9a55c2216999c362
BearandYoon <luckysevenbear@gmail.com>
2018-02-10 04:11:18 +0100

    fix style issue

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -5,6 +5,7 @@ import { AuthService } from '../../core/auth/auth.service';
 @Injectable()
 export class AuthGuardService implements CanActivate {
     constructor(public auth: AuthService, public router: Router) {}
+
     canActivate(): boolean {
         if (!this.auth.isLoggedIn()) {
             this.router.navigate(['auth']);
