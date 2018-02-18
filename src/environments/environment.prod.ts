130abab8251b01045d7b9c0b1f78970137f5f916
BearandYoon <luckysevenbear@gmail.com>
2018-02-19 02:03:49 +0400

    api version 21

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://api.amalyze.com/0.0.19',
-    apiVersion: '0.0.19',
+    baseUrl: 'https://api.amalyze.com/0.0.21',
+    apiVersion: '0.0.21',
     version: '0.0.11',
     googleSiteKey: '6LcKNEIUAAAAAPGe3VDGzUcqXyi5iyMD-IPhiXej'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://api.amalyze.com/0.0.19',
-  apiVersion: '0.0.19',
+  baseUrl: 'https://api.amalyze.com/0.0.21',
+  apiVersion: '0.0.21',
   version: '0.0.11',
   googleSiteKey: '6LcKNEIUAAAAAPGe3VDGzUcqXyi5iyMD-IPhiXej'
 };
